8f7c6a95446986940eabc91577d8d5c75315e310
or <orshapira91@gmail.com>
2017-11-09 09:55:23 +0200

    :)

--- src/app/social.service.ts
+++ src/app/social.service.ts
@@ -7,7 +7,7 @@ export class socialService {
 		return ([
 		{
 			name: "Bitcoin",
-			img: "img/coins.jpg",
+			img: "img/bitcoin.jpg",
 			events: [
 			{
 				id: 1,
@@ -26,9 +26,33 @@ export class socialService {
 					comments: 65,
 					sentiment : 0
 				},{
+					eventId : "uniqe",
+					message : "helldsfaasdfwo",
+					publisher: "2324ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "unsdfasdfiqe",
+					message : "hesdafasdllo",
+					publisher: "ar23i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe",
+					message : "h23232ello",
+					publisher: "ar252i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
 					eventId : "uniqe4",
 					message : "hello4",
-					publisher: "ari",
+					publisher: "a23432ri",
 					datetime : 1509970442,
 					liks: 4,
 					comments: 65,
@@ -53,7 +77,24 @@ export class socialService {
 				}]
 			},{
 				id: 3,
-				datetime: 1509950942,
+				datetime: 1509951342,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 5,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950932,
 				networkId: 1,
 				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
 				totalLikes: 800,
@@ -68,35 +109,497 @@ export class socialService {
 					comments: 65,
 					sentiment : 0
 				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helasdfaslo",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hel24lo",
+					publisher: "a2232ri",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helsgaaglo",
+					publisher: "ar3234i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950842,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 6,
+				messages: [{
+					eventId : "uniqe",
+					message : "hasdfello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
 			}]
 		},
 		{
 			name: "Ethereum", 
-			img: "img/coins.jpg"
+			img: "img/ethereum.jpg",
+			events: [
+			{
+				id: 1,
+				datetime: 1509950442,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 2,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe",
+					message : "helldsfaasdfwo",
+					publisher: "2324ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "unsdfasdfiqe",
+					message : "hesdafasdllo",
+					publisher: "ar23i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe",
+					message : "h23232ello",
+					publisher: "ar252i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe4",
+					message : "hello4",
+					publisher: "a23432ri",
+					datetime : 1509970442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 2,
+				datetime: 1509951442,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 5,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 3,
+				datetime: 1509951342,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 5,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950932,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 2,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helasdfaslo",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hel24lo",
+					publisher: "a2232ri",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helsgaaglo",
+					publisher: "ar3234i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950842,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 6,
+				messages: [{
+					eventId : "uniqe",
+					message : "hasdfello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			}]
 		},
 		{
 			name: "Ripple", 
-			img: "img/coins.jpg"
+			img: "img/ripple.jpg",events: [
+			{
+				id: 1,
+				datetime: 1509950442,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 2,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe",
+					message : "helldsfaasdfwo",
+					publisher: "2324ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "unsdfasdfiqe",
+					message : "hesdafasdllo",
+					publisher: "ar23i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe",
+					message : "h23232ello",
+					publisher: "ar252i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				},{
+					eventId : "uniqe4",
+					message : "hello4",
+					publisher: "a23432ri",
+					datetime : 1509970442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 2,
+				datetime: 1509951442,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 5,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 3,
+				datetime: 1509951342,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 5,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950932,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 2,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helasdfaslo",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hel24lo",
+					publisher: "a2232ri",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509951942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "helsgaaglo",
+					publisher: "ar3234i",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950942,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 8,
+				messages: [{
+					eventId : "uniqe",
+					message : "hello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			},{
+				id: 4,
+				datetime: 1509950842,
+				networkId: 1,
+				networkIco: "http://site.je/app/img/nucleo-social-icons/svg/social-1_logo-twitter.svg",
+				totalLikes: 800,
+				sentiment : 0,
+				messagesCount : 6,
+				messages: [{
+					eventId : "uniqe",
+					message : "hasdfello",
+					publisher: "ari",
+					datetime : 1509950442,
+					liks: 4,
+					comments: 65,
+					sentiment : 0
+				}]
+			}]
 		},
 		{
 			name: "Litecoin", 
-			img: "img/coins.jpg"
+			img: "img/litecoin.jpg"
 		},
 		{
 			name: "Dash", 
-			img: "img/coins.jpg"
-		},
-		{
-			name: "NEO", 
-			img: "img/coins.jpg"
+			img: "img/dash.jpg"
 		},
 		{
 			name: "NEM", 
-			img: "img/coins.jpg"
+			img: "img/nem.jpg"
 		},
 		{
-			name: "Qtum",
-			img: "img/coins.jpg"
+			name: "Siacoin", 
+			img: "img/siacoin.jpg"
+		},
+		{
+			name: "Monero",
+			img: "img/monero.jpg"
+		},
+		{
+			name: "Bitcoincash",
+			img: "img/bitcoincash.jpg"
 		}]);
 	}
 }
